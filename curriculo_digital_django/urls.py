9a702561234a6734410fd9d823517fb72dfd90f5
andrelccorrea <andre.lcosta@hotmail.com>
2022-06-14 23:12:48 -0300

    fix: urls.py imports

--- curriculo_digital_django/urls.py
+++ curriculo_digital_django/urls.py
@@ -17,9 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +24,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )  # type: ignore
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
